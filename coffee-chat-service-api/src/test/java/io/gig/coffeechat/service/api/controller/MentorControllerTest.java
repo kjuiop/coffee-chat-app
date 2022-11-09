09979aa54f0c5fd560124253e350cc556534b491
kjuiop <copying_y@naver.com>
2022-11-09 18:33:11 +0900

    [#7] test: mentor sign-up test

--- coffee-chat-service-api/src/test/java/io/gig/coffeechat/service/api/controller/MentorControllerTest.java
+++ coffee-chat-service-api/src/test/java/io/gig/coffeechat/service/api/controller/MentorControllerTest.java
@@ -1,6 +1,7 @@
 package io.gig.coffeechat.service.api.controller;
 
 import io.gig.coffeechat.service.api.config.ServiceApiTestConfig;
+import io.gig.coffeechat.service.api.dto.mentor.MentorDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
@@ -24,15 +25,35 @@ class MentorControllerTest extends ServiceApiTestConfig {
     public void mentorSignUpTest() throws Exception {
 
         // given
+        MentorDto.DetailInfo mentorDetailInfo = MentorDto.DetailInfo.builder()
+                .schoolName("서울대학교")
+                .major("경영학과")
+                .year(4)
+                .studentNo("202034735")
+                .build();
+
+        MentorDto.SignUpRequest request = MentorDto.SignUpRequest
+                .builder()
+                .email("arneg0shua@gmail.com")
+                .nickname("jake")
+                .gender("M")
+                .usageAuthority("MENTOR")
+                .birth(LocalDate.of(1992,8,25))
+                .mentorDetailInfo(mentorDetailInfo)
+                .build();
+
+        String content = convertJsonToString(request);
 
         // when
-        ResultActions result = mockMvc.perform(post("/api/members/mentors/1/sign-up")
+        ResultActions result = mockMvc.perform(post("/api/members/mentors/4/sign-up")
+                .content(content)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON));
 
         // then
         result.andExpect(status().isCreated())
                 .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.data.signUpToken").value(4))
         ;
     }
 
