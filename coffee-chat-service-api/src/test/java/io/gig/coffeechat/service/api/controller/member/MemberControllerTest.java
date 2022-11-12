3caad6d2bfa583374eca85a020273af6979a0259
kjuiop <copying_y@naver.com>
2022-11-12 11:36:31 +0900

    [#12] test: nickname validation test add

--- coffee-chat-service-api/src/test/java/io/gig/coffeechat/service/api/controller/member/MemberControllerTest.java
+++ coffee-chat-service-api/src/test/java/io/gig/coffeechat/service/api/controller/member/MemberControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -33,6 +32,27 @@ public class MemberControllerTest extends ServiceApiTestConfig {
 
         // when
         ResultActions result = mockMvc.perform(get("/api/email-verify")
+                .params(request)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.data.isValid").value(true));
+
+    }
+
+    @DisplayName("닉네임 중복 체크 테스트")
+    @Test
+    public void validNicknameTest() throws Exception {
+
+        // given
+        MultiValueMap<String, String> request = new LinkedMultiValueMap<>();
+        request.add("nickname", "jake12");
+
+        // when
+        ResultActions result = mockMvc.perform(get("/api/nickname-verify")
                 .params(request)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON));
