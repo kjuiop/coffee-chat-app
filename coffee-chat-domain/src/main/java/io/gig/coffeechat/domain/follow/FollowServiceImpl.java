424d76280d1268f3c060f06495d7c9c4fbc4ead9
kjuiop <copying_y@naver.com>
2022-11-19 20:08:50 +0900

    [#27] feat: follow request 간 동일한 회원 uuid 여부 validation 추가

--- coffee-chat-domain/src/main/java/io/gig/coffeechat/domain/follow/FollowServiceImpl.java
+++ coffee-chat-domain/src/main/java/io/gig/coffeechat/domain/follow/FollowServiceImpl.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.security.InvalidParameterException;
+
 /**
  * @author : JAKE
  * @date : 2022/11/19
@@ -19,7 +21,14 @@ public class FollowServiceImpl implements FollowService {
     @Override
     @Transactional
     public void create(MemberInfo.Main fromMember, MemberInfo.Main toMember) {
+        validateFollowRequest(fromMember.getUuid(), toMember.getUuid());
         Follow newFollow = Follow.CreateFollow(fromMember, toMember);
         followStore.store(newFollow);
+    }
+
+    private void validateFollowRequest(String fromUuid, String toUuid) {
+        if (fromUuid.equals(toUuid)) {
+            throw new InvalidParameterException("팔로우 요청 간 회원들의 uuid 가 동일할 수 없습니다. from uuid : " + fromUuid + " to uuid : " + toUuid);
+        }
     }
 }
