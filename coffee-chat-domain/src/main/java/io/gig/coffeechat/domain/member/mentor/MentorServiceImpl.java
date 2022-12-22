03b6c8ca5627302100bd342abdc671e2a0eaeb69
kjuiop <copying_y@naver.com>
2022-12-23 00:16:54 +0900

    [#51] feat: mentor change school-name DAO 기능개발

--- coffee-chat-domain/src/main/java/io/gig/coffeechat/domain/member/mentor/MentorServiceImpl.java
+++ coffee-chat-domain/src/main/java/io/gig/coffeechat/domain/member/mentor/MentorServiceImpl.java
@@ -1,7 +1,10 @@
 package io.gig.coffeechat.domain.member.mentor;
 
+import io.gig.coffeechat.domain.member.Member;
+import io.gig.coffeechat.domain.member.MemberReader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author : JAKE
@@ -11,8 +14,20 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MentorServiceImpl implements MentorService {
 
+    private final MentorStore mentorStore;
+    private final MemberReader memberReader;
+
     @Override
+    @Transactional
     public boolean changeSchoolName(String uuid, MentorCommand.ChangeSchoolName request) {
+
+        // 회원이 Mentor 인지 Validation
+
+        Member findMember = memberReader.getMember(uuid);
+        MentorDetail findMentorDetail = findMember.getMentorDetail();
+        findMentorDetail.changeSchoolName(request.getSchoolName());
+        mentorStore.store(findMentorDetail);
+
         return true;
     }
 
